24a3441ba08ed89346b47e0fd5a545490bd7c21b
Luo Sheng <luosheng1986@gmail.com>
2016-06-08 20:37:27 +0800

    Add iOS example project and add DontShoot as an embedded framework.

---
+++ Example-iOS/Example-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by Luo Sheng on 16/6/8.
+//  Copyright © 2016年 Luo Sheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
