2600a6e9f05f7d1dcdd96c18a45b2d33e180f61f
Luo Sheng <luosheng1986@gmail.com>
2016-06-08 23:04:37 +0800

    Add basic elements from SpriteKit.

---
+++ DontShoot/DontShoot/BarrageNode.swift
@@ -0,0 +1,14 @@
+//
+//  BarrageNode.swift
+//  DontShoot
+//
+//  Created by Luo Sheng on 16/6/8.
+//  Copyright © 2016年 Luo Sheng. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+public class BarrageNode: SKLabelNode {
+    
+}
