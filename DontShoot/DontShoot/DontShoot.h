8e439f40690c8d1646e4783fea4559cec585b95c
Luo Sheng <luosheng1986@gmail.com>
2016-06-08 20:28:05 +0800

    Initial commit.

---
+++ DontShoot/DontShoot/DontShoot.h
@@ -0,0 +1,19 @@
+//
+//  DontShoot.h
+//  DontShoot
+//
+//  Created by Luo Sheng on 16/6/8.
+//  Copyright © 2016年 Luo Sheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DontShoot.
+FOUNDATION_EXPORT double DontShootVersionNumber;
+
+//! Project version string for DontShoot.
+FOUNDATION_EXPORT const unsigned char DontShootVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DontShoot/PublicHeader.h>
+
+
