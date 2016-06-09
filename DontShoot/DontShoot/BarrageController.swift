5bd20309e2db3f9f81560f315a97a413733341c2
Luo Sheng <luosheng1986@gmail.com>
2016-06-09 08:50:04 +0800

    Setup sprite view and scene.

--- DontShoot/DontShoot/BarrageController.swift
+++ DontShoot/DontShoot/BarrageController.swift
@@ -11,4 +11,26 @@ import SpriteKit
 
 public class BarrageController {
     
+    let spriteView: SKView
+    
+    public init() {
+        spriteView = SKView()
+        
+        spriteView.allowsTransparency = true
+        spriteView.showsFPS = true
+        spriteView.showsNodeCount = true
+        spriteView.showsDrawCount = true
+        spriteView.backgroundColor = SKColor.clearColor()
+    }
+    
+    public func renderInView(view: UIView) {
+        spriteView.frame = view.bounds
+        view.addSubview(spriteView)
+        
+        let scene = BarrageScene(size: spriteView.bounds.size)
+        spriteView.presentScene(scene)
+        scene.backgroundColor = SKColor.clearColor()
+        scene.scaleMode = .AspectFit
+    }
+    
 }
